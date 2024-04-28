c8aab7cb8b11c6838e074da2f49efd339d6c5b46
Pravesh Yadav <yadavpravesh776@gmail.com>
2024-04-28 20:26:49 +0530

    Group No 16 Music Genre Recognizer

--- Group 7 MiniProject - AQI Tracker/Breatheasy/Breatheasy/AQI/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from import_export.admin import  ImportExportModelAdmin
-from AQI.models import StudentUser,Location
-from . import models
-# Register your models here.
-
-
-admin.site.register(StudentUser)
-admin.site.register(Location, ImportExportModelAdmin)
